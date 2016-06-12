4aea0f6b4b06f763a19d78e4a19aabe2740bde3b
blaze-bnayak <blazebnayak@gmail.com>
2016-06-12 10:02:54 +0545

    Update MainActivity.java

--- Calculator/app/src/main/java/com/rent/blaze/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/rent/blaze/calculator/MainActivity.java
@@ -2,13 +2,11 @@ package com.rent.blaze.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
